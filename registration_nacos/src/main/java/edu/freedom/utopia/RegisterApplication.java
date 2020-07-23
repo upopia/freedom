aa277506ece418bf1cae3c68e52b8f52ed59bb59
lihj <hongjie.li@duyansoft.com>
2020-07-23 11:41:44 +0800

    初次提交

---
+++ registration_nacos/src/main/java/edu/freedom/utopia/RegisterApplication.java
@@ -0,0 +1,16 @@
+package edu.freedom.utopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: lihj
+ * @Date: 2020/7/22 8:36 下午
+ */
+
+@SpringBootApplication
+public class RegisterApplication {
+    public static void main(String[] args){
+        SpringApplication.run(RegisterApplication.class);
+    }
+}
