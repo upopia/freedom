dbff42246b2bb139b33d277fe77493522fa4520d
lihj <hongjie.li@duyansoft.com>
2020-07-31 16:00:19 +0800

    提交一些代码

--- common/src/main/java/edu/freedom/utopia/config/swagger2/Swagger2Config.java
+++ common/src/main/java/edu/freedom/utopia/config/swagger2/Swagger2Config.java
@@ -21,7 +21,7 @@ public class Swagger2Config {
         return new Docket(DocumentationType.SWAGGER_2)
                 .pathMapping("/")
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.nvn.controller"))
+                .apis(RequestHandlerSelectors.basePackage("edu.freedom.utopia"))
                 .paths(PathSelectors.any())
                 .build().apiInfo(new ApiInfoBuilder()
                         .title("自由")
